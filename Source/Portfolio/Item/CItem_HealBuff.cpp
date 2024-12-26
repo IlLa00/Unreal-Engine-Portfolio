5eedbbb185671a1d90548214eb90e0e795a61a6c
김현준 <154495800+IlLa00@users.noreply.github.com>
2024-12-26 17:42:02 +0900

    1226_
    아이템 비긴오버랩 문제 해결했으나 파티클 재생이안됨. 에셋이 빠져있음 왜?

--- Source/Portfolio/Item/CItem_HealBuff.cpp
+++ Source/Portfolio/Item/CItem_HealBuff.cpp
@@ -10,7 +10,10 @@ ACItem_HealBuff::ACItem_HealBuff()
 
 void ACItem_HealBuff::BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	CLog::Print(OtherComp->GetName()); // 브러쉬 컴포넌트랑 부딪히는데 이게뭐임???????????
+	CLog::Print(OtherActor->GetName()); 
+	CLog::Print(OtherComp->GetName()); 
+
+	// 왜 파티클 재생이안댐?
 
 	// 맞은 대상에게 파티클 재생
 	if(SpawnParticle)
